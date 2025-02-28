a95fb55a28619a7da77d78ac48d6550b63bff2b5
Vaibhav Singh <vaibhav.singh@philips.com>
2025-02-28 15:47:45 +0530

    code

---
+++ src/test/java/com/philips/cicd/GithubCicdActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.philips.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCicdActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
